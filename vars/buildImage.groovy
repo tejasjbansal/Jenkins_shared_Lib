27c3b4a4e0aaa570d2a99321ec291694a4a73266
Tejash Bansal <56173595+tejasjbansal@users.noreply.github.com>
2023-02-24 00:28:05 +0530

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 
 def call(){
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
+   echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'Docker-Id', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t tejashbansal/my-repo:jma-3.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
+        sh 'docker push tejashbansal/my-repo:jma-3.0'
     }
 }
